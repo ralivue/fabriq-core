b67e3bf8b704bfd01bdd7a869fb2ab8e1c4ef816
Will Steinmetz <willsteinmetz@me.com>
2014-12-07 16:15:02 -0500

    Started the session class

---
+++ session.class.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @file session.class.php
+ * @author Will Steinmetz
+ * Fabriq class for managing sessions
+ */
+
+namespace Fabriq\Core {
+  class Session {
+    /**
+     *
+     */
+  }
+}
